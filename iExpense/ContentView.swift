9eea3c212f38568f875ec44a4268bceed8446f49
zoli0829 <zoli0829@gmail.com>
2025-04-15 10:33:19 +0100

    Initial commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Zoltan Vegh on 15/04/2025.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Zoltan Vegh on 15/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
